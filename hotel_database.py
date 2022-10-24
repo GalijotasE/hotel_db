066e6ef275baf6b743316f4606108fff5e213d98
GalijotasE <galijotase@gmail.com>
2022-10-25 01:05:08 +0300

    click here to see recent changes

--- hotel_database.py
+++ hotel_database.py
@@ -18,6 +18,19 @@ class Hotel_Guest(Base):
     room_no = Column("room_no", Integer, ForeignKey("hotel_room.room_no"))
     booking_id = Column("booking_id", Integer)
 
+    def __init__(self, guest_f_name, guest_l_name, reason_of_stay, requests, payment_id, loyalty, room_no, booking_id):
+        self.guest_f_name = guest_f_name
+        self.guest_l_name = guest_l_name
+        self.reason_of_stay = reason_of_stay
+        self.requests = requests
+        self.payment_id = payment_id
+        self.loyalty = loyalty
+        self.room_no = room_no
+        self.booking_id = booking_id
+
+    def __repr__(self):
+        return f"({self.id}, {self.guest_f_name}, {self.guest_l_name}, {self.reason_of_stay}, {self.requests}, {self.payment_id}, {self.loyalty}, {self.room_no}, {self.booking_id})"
+
 
 class Hotel_Room(Base):
     __tablename__ = "hotel_room"
@@ -27,12 +40,28 @@ class Hotel_Room(Base):
     nightly_rate = Column("nightly_rate", Float, ForeignKey("room_type.nightly_rate"))
     booking_id = Column("booking_id", Integer, ForeignKey("booking.id"))
 
+    def __init__(self, room_type, guest_id, nightly_rate, booking_id):
+        self.room_type = room_type
+        self.guest_id = guest_id
+        self.nightly_rate = nightly_rate
+        self.booking_id = booking_id
+    
+    def __repr__(self):
+        return f"({self.room_no}, {self.room_type}, {self.guest_id}, {self.nightly_rate}, {self.booking_id})"
+
 
 class Room_Type(Base):
     __tablename__ = "room_type"
     id = Column(Integer, primary_key = True)
     type_of_room = Column("type_of_room", String)
     nightly_rate = Column("nightly_rate", Float)
+
+    def __init__(self, type_of_room, nightly_rate):
+        self.type_of_room = type_of_room
+        self.nightly_rate = nightly_rate
+
+    def __repr__(self):
+        return f"({self.id}, {self.type_of_room}, {self.nightly_rate})"
 
 
 class Booking(Base):
@@ -48,6 +77,20 @@ class Booking(Base):
     nights = Column("nights", Integer)
     total = Column("total_price", Float)
 
+    def __init__(self, guest_id, payment_id, room_no, check_in, check_out, number_of_guests, nightly_rate, nights, total):
+        self.guest_id = guest_id
+        self.payment_id = payment_id
+        self.room_no = room_no
+        self.check_in = check_in
+        self.check_out = check_out
+        self.number_of_guests = number_of_guests
+        self.nightly_rate = nightly_rate
+        self.nights = nights
+        self.total = total
+
+    def __repr__(self):
+        return f"({self.id}, {self.guest_id}, {self.payment_id}, {self.room_no}, {self.check_in}, {self.check_out}, {self.number_of_guests}, {self.nightly_rate}, {self.nights}, {self.total})"
+
 
 class Payment(Base):
     __tablename__ = "payment"
@@ -58,6 +101,16 @@ class Payment(Base):
     nightly_rate = Column("nightly_rate", Float, ForeignKey("room_type.nightly_rate"))
     total = Column("total", Float)
 
+    def __init__(self, guest_id, payment_method, booking_id, nightly_rate, total):
+        self.guest_id = guest_id
+        self.payment_method = payment_method
+        self.booking_id = booking_id
+        self.nightly_rate = nightly_rate
+        self.total = total
+
+    def __repr__(self):
+        return f"({self.id}, {self.guest_id}, {self.payment_method}, {self.booking_id}, {self.nightly_rate}, {self.total})"
+
 
 class Transaction(Base):
     __tablename__ = "transaction"
@@ -67,6 +120,16 @@ class Transaction(Base):
     total = Column("total", Float, ForeignKey("payment.total"))
     acc_no = Column("acc_no", String)
     payment_status = Column("payment_status", String)
+
+    def __init__(self, guest_id, payment_method, total, acc_no, payment_status):
+        self.guest_id = guest_id
+        self.payment_method = payment_method
+        self.total = total
+        self.acc_no = acc_no
+        self.payment_status = payment_status
+
+    def __repr__(self):
+        return f"({self.id}, {self.guest_id}, {self.payment_method}, {self.total}, {self.acc_no}, {self.payment_status}"
 
 
 
