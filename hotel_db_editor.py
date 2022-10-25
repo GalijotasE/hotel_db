c0eb41f61e57edb405625be493382ec2109b127b
GalijotasE <galijotase@gmail.com>
2022-10-25 12:02:32 +0300

    click here to see recent changes

--- hotel_db_editor.py
+++ hotel_db_editor.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
-#### VIEW DATA FROM THE DATABASE
+########## VIEW DATA FROM THE DATABASE
 
 def show_all_hotel_guest():
     print("---List of Hotel Guests---")
@@ -42,18 +42,130 @@ def show_all_transaction():
     for transaction in transactions:
         print(transaction)
     
-#### ADD DATA TO THE DATABASE
+########## ADD DATA TO THE DATABASE
 
 
-# def edit_all_hotel_guest():
-#     print("---Editor of Hotel Guests---")
-#     try:
-#        f_name = input("first name: ")
-#        l_name = input("last name: ")
-#        reasons = input("reasons of stay: ")
-#        requests = input("requests: ")
-#        payment_id = input("choose payment id")
-       
+def edit_all_hotel_guest():
+    print("---Editor Of Hotel Guests---")
+    try:
+       f_name = input("first name: ")
+       l_name = input("last name: ")
+       reason = input("reason of stay: ")
+       request = input("requests: ")
+       print("Please choose payment id")
+       show_all_payment()
+       payment = input("payment id: ")
+       loyalty_g = input("loyalty: ")
+       print("Please choose room number")
+       show_all_hotel_room()
+       room_number = input("room number: ")
+       print("Please choose booking")
+       show_all_booking()
+       booking = input("booking id: ")
+    except ValueError:
+        print("ERROR!!!")
+    else:
+        new_guest = Hotel_Guest(
+            guest_f_name= f_name,
+            guest_l_name= l_name,
+            reason_of_stay= reason,
+            requests= request,
+            payment_id= payment,
+            loyalty= loyalty_g,
+            room_no= room_number,
+            booking_id= booking
+        )
+        session.add(new_guest)
+        session.commit()
+        print(f"New Guest {f_name} has been added to the database.")
+
+
+def edit_all_hotel_room():
+    print("---Editor Of Hotel Rooms---")
+    try:
+        print("Choose Room Type Id")
+        show_all_room_type()
+        type_of_room = input("Room Type Id: ")
+        print("Choose Guest Id")
+        show_all_hotel_guest()
+        g_id = input("Guest Id: ")
+        print("Choose Nightly Rate")
+        show_all_room_type()
+        night_rate = input("Nightly rate: ")
+        print("Choose Booking Id")
+        show_all_booking()
+        booking = input("Booking Id: ")
+    except ValueError:
+        print("ERROR!!!")
+    else:
+        new_room = Hotel_Room(
+            room_type= type_of_room,
+            guest_id= g_id,
+            nightly_rate= night_rate,
+            booking_id= booking
+        )
+        session.add(new_room)
+        session.commit()
+        print(f"New room {type_of_room} has been added to the database.")
+
+
+def edit_all_room_type():
+    print("---Editor Of Hotel Room Types---")
+    try:
+        room_type = input("Type Of Room: ")
+        night_rate = input("Night Rate: ")
+    except ValueError:
+        print("ERROR!!!")
+    else:
+        new_type = Room_Type(
+            type_of_room= room_type,
+            nightly_rate= night_rate
+        )
+        session.add(new_type)
+        session.commit()
+        print(f"New room type {room_type} has been added to the database")
+
+
+def edit_all_booking():
+    print("---Editor Of Hotel Bookings---")
+    try:
+        print("Choose guest id")
+        show_all_hotel_guest()
+        guest = input("Guest id: ")
+        print("Choose payment id")
+        show_all_payment()
+        payment = input("Payment id: ")
+        print("Choose room number:")
+        show_all_hotel_room()
+        rooms = input("Room number: ")
+        c_in = input("Check in: ")
+        c_out = input("Check out: ")
+        no_of_guests = input("Number Of Guests: ")
+        print("Choose nightly rate")
+        show_all_room_type()
+        night_rate = input("Nightly rate: ")
+        night = input("Nights: ")
+        total = input("Total: ")
+    except ValueError:
+        print("ERROR!!!")
+    else:
+        new_booking = Booking(
+            guest_id= guest,
+            payment_id= payment,
+            room_no= rooms,
+            check_in= c_in,
+            check_out= c_out,
+            number_of_guests= no_of_guests,
+            nightly_rate= night_rate,
+            nights= night,
+            total= total
+        )
+        session.add(new_booking)
+        session.commit()
+        print(f"New booking for {guest} has been added to the database.")
+
+
+########## Infinite menu cycle
 
 while True:
     print("---HOTEL DATABASE EDITOR---")
@@ -63,6 +175,8 @@ while True:
     print("d - Delete Data In The Database")
     print("q - Quit")
     action = input("please choose: ").casefold()
+
+    #### adding data cycle
     if action == "a":
         print("---Adding Data To The Database---")
         print("g - Add New Hotel Guest")
@@ -74,11 +188,11 @@ while True:
         print("q - Quit")
         choice = input("please choose: ").casefold()
         if choice == "g":
-            pass
+            edit_all_hotel_guest()
         elif choice == "r":
-            pass
+            edit_all_hotel_room()
         elif choice == "t":
-            pass
+            edit_all_room_type()
         elif choice == "b":
             pass
         elif choice == "p":
@@ -89,6 +203,8 @@ while True:
             break
         else:
             print("ERROR!!! Please Choose Only From Given Inputs!!!")
+            
+    #### Viewing data cycle
     elif action == "v":
         print("---Viewing Data From The Database---")
         print("g - View New Hotel Guest")
