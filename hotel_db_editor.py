1ae77b152a386b6d45a8f4ebd868fed5895bad23
GalijotasE <galijotase@gmail.com>
2022-10-25 11:17:42 +0300

    click here to see recent changes

--- hotel_db_editor.py
+++ hotel_db_editor.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
+#### VIEW DATA FROM THE DATABASE
 
 def show_all_hotel_guest():
     print("---List of Hotel Guests---")
@@ -41,7 +42,7 @@ def show_all_transaction():
     for transaction in transactions:
         print(transaction)
     
-
+#### ADD DATA TO THE DATABASE
 
 
 # def edit_all_hotel_guest():
@@ -61,6 +62,56 @@ while True:
     print("v - View Data In The Database")
     print("d - Delete Data In The Database")
     print("q - Quit")
-    action = input("please choose: ")
+    action = input("please choose: ").casefold()
     if action == "a":
         print("---Adding Data To The Database---")
+        print("g - Add New Hotel Guest")
+        print("r - Add New Hotel Room")
+        print("t - Add New Hotel Room Type")
+        print("b - Add New Booking")
+        print("p - Add New Payment")
+        print("t - Add New Transaction")
+        print("q - Quit")
+        choice = input("please choose: ").casefold()
+        if choice == "g":
+            pass
+        elif choice == "r":
+            pass
+        elif choice == "t":
+            pass
+        elif choice == "b":
+            pass
+        elif choice == "p":
+            pass
+        elif choice == "t":
+            pass
+        elif choice == "q":
+            break
+        else:
+            print("ERROR!!! Please Choose Only From Given Inputs!!!")
+    elif action == "v":
+        print("---Viewing Data From The Database---")
+        print("g - View New Hotel Guest")
+        print("r - View New Hotel Room")
+        print("t - View New Hotel Room Type")
+        print("b - View New Booking")
+        print("p - View New Payment")
+        print("t - View New Transaction")
+        print("q - Quit")
+        choice = input("Please Choose: ").casefold()
+        if choice == "g":
+            show_all_hotel_guest()
+        elif choice == "r":
+            show_all_hotel_room()
+        elif choice == "t":
+            show_all_room_type()
+        elif choice == "b":
+            show_all_booking()
+        elif choice == "p":
+            show_all_payment()
+        elif choice == "t":
+            show_all_transaction()
+        elif choice == "q":
+            break
+        else:
+            print("ERROR!!! Please Choose Only From Given Inputs!!!")
